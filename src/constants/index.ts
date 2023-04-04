e0f8ab2aa9943167f250c4a38128ec100b386c4a
wei-hong <zxc37984@gmail.com>
2023-04-04 20:07:08 +0800

    feat: add refetch stock and show input to change display stock

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,6 @@
+export const TW_STOCK_API_PREFIX =
+  "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=";
 export const TW_STOCK_MONITOR_INPUT = "tw-stock-monitor.input";
 export const TW_STOCK_COLOR_RED = "#ff333a";
 export const TW_STOCK_COLOR_GREEN = "#00ab5e";
+export const TW_STCK_DEFAULT_REFETCH_INTERVAL = 5000;
